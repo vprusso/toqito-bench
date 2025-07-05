4ccca2377a76ab86061b41e848e6463b25e5d611
RajdeepAher <aher_rp@ma.iitr.ac.in>
2025-07-05 15:50:18 +0530

    fix

--- setup/test_qetlab.m
+++ setup/test_qetlab.m
@@ -5,12 +5,6 @@ disp('--- QETLAB CI Test Script ---');
 
 % Add QETLAB and CVX to the path
 addpath(genpath('QETLAB/QETLAB-0.9'));
-addpath(genpath('cvx/cvx'));
-
-% Set up CVX
-cd('cvx/cvx');
-cvx_setup;
-cd('../../');
 
 rho_rand = RandomDensityMatrix(9);
 is_ppt = IsPPT(rho_rand);
