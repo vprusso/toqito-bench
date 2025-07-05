afb9cc777ad3de249f0c0cd12b741920c4f676e7
RajdeepAher <aher_rp@ma.iitr.ac.in>
2025-07-05 17:30:42 +0530

    fix: now running benchmarks in CI

--- setup/test_qetlab.m
+++ setup/test_qetlab.m
@@ -6,9 +6,27 @@ disp('--- QETLAB CI Test Script ---');
 % Add QETLAB and CVX to the path
 addpath(genpath('QETLAB/QETLAB-0.9'));
 
-rho_rand = RandomDensityMatrix(9);
-is_ppt = IsPPT(rho_rand);
-disp(['Random 9x9 state is PPT: ', num2str(is_ppt)]);
-
+classdef PartialTraceBenchmarks < matlab.perftest.TestCase
+    properties (TestParameter)
+         matrix_size = struct( ...
+            'size_4', 4, ...
+            'size_16', 16, ...
+            'size_64', 64, ...
+            'size_256', 256 ...
+        );
+    end
+    
+    methods (Test)
+        function test_bench__partial_trace__vary__input_mat(testCase, matrix_size)
+            input_mat = randn(matrix_size, matrix_size) + 1i*randn(matrix_size, matrix_size)
+            sys = 2;
+            dim = [];
+            testCase.startMeasuring();
+            result = PartialTrace(input_mat, sys, dim)
+            testCase.stopMeasuring();
+            testCase.verifyLessThanOrEqual(size(result,1), matrix_size);
+        end
+    end
+end
 
 disp('--- End of QETLAB CI Test Script ---');
